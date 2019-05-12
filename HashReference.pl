066b1786ab3d2b8b8ea73c0e97a77a584c6946fc
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-05-12 19:34:16 -0400

    debugging

--- HashReference.pl
+++ HashReference.pl
@@ -9,15 +9,50 @@
 
 use strict;
 use warnings;
+use v5.13.7;
 
 
-my %price_of;
-my $price;
+my %hashRef;
+my $key1;
+my $key2;
+my $value1;
+my $value2;
 
-%price_of = ("car",{
-		"Cheap", "3000",
-		"Pricey", "30000",},);
+
+print "Add Keys to Cars Hash \n";
+print "1st Key: ";
+$key1 = <STDIN>;
+chomp $key1;
+print "\n";
+
+print "2nd Key: ";
+$key2 = <STDIN>;
+chomp $key2;
+print "\n";
+
+print "Add Values To the Keys \n";
+print "Value of 1st Key: ";
+$value1 = <STDIN>;
+chomp $value1;
+print "\n";
+
+print "Value of 2nd Key: ";
+$value2 = <STDIN>;
+chomp $value2;
+print "\n";
+
+
+
+
+
+%hashRef = ("Cars", {
+		"$key1", "$value1",
+		"$key2", "$value2",},);
 		
-$price = $price_of{car};
-print "$price->{Cheap}";
+my $hashRef = %hashRef;
+
+
+foreach my $hashRef (keys %hashRef){
+		print "$hashRef{$key} \n";
+		}
 
